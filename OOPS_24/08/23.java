34d80b07e2c9af0f4d027f30a6487cb9b52db318
Roti-cyber <64833632+Roti-cyber@users.noreply.github.com>
2023-08-24 06:26:22 +0000

    Program completed

---
+++ OOPS_24/08/23.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Sibi{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter alcohol level:");
+        int alc=sc.nextInt();
+        if (alc>100)
+        {
+            System.out.println("Sibi Drunk!! :)");
+        }
+        else
+        {
+            System.out.println("Good boi:Sibi not drunk :))");
+        }
+
+        
+    }
+}
