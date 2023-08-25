71130aaf1881c088b6ea7f191ded8cc748730b9e
Roti-cyber <64833632+Roti-cyber@users.noreply.github.com>
2023-08-25 06:27:51 +0000

    Completed

---
+++ Program_6.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class DigitSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int num = sc.nextInt();
+        
+        int evenSum = 0;
+        int oddSum = 0;
+        
+        while (num > 0) {
+            int digit = num % 10;
+            
+            if (digit % 2 == 0) {
+                evenSum += digit;
+            } else {
+                oddSum += digit;
+            }
+            
+            num /= 10;
+        }
+        
+        System.out.println("Sum of even digits: " + evenSum);
+        System.out.println("Sum of odd digits: " + oddSum);
+        
+        sc.close();
+    }
+}
