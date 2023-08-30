1d7dce00d3295c778bb8227a94f8c802b650fa2e
Roti-cyber <64833632+Roti-cyber@users.noreply.github.com>
2023-08-30 10:22:22 +0000

    continue completed

---
+++ continue_break.java
@@ -0,0 +1,14 @@
+class continue_break
+{
+   public static void main(String args[]) 
+   {
+    for(int i=0;i<5;i++)
+    {
+        if(i==2)
+        {
+          continue; 
+        }
+         System.out.println(i);
+    }
+   }
+}
