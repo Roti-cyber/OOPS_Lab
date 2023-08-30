9d52eeba8f41298209a90b55b18d30db1969639c
Roti-cyber <64833632+Roti-cyber@users.noreply.github.com>
2023-08-30 10:48:11 +0000

    Completed

---
+++ loop.java
@@ -0,0 +1,18 @@
+class LOOP
+{
+    public static void main(String args[])
+    {
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<5;j++)
+            {
+                System.out.print("*");
+                if(j==2)
+                {
+                    break;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
