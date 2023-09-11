0aa5fd82a343b31a64c1f75d06c65f024f42e007
Roti-cyber <64833632+Roti-cyber@users.noreply.github.com>
2023-09-11 04:15:11 +0000

    Completed

---
+++ Program_1_5.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Array
+{
+    public static void main(String args[])
+    {
+        int num=0,sum=0;
+        Scanner sc=new Scanner(System.in);
+        int[] array=new int[5];
+        for(int i=0;i<5;i++)
+        {
+            System.out.println("Enter a number:");
+            num=sc.nextInt();
+            sum=sum+num;
+        }
+        System.out.println("Sum of numbers is:"+sum);
+        
+    }
+}
